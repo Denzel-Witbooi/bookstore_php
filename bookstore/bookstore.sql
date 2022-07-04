dbaae538c1b0e6de665812c0ea3a34b05bbf4d7f
Denzel Witbooi <77748858+Denzel-Witbooi@users.noreply.github.com>
2022-07-04 18:10:04 +0200

    Add files via upload

--- bookstore/bookstore.sql
+++ bookstore/bookstore.sql
@@ -3,9 +3,9 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Jul 03, 2022 at 11:26 PM
+-- Generation Time: Jul 04, 2022 at 06:05 PM
 -- Server version: 10.4.22-MariaDB
--- PHP Version: 7.4.27
+-- PHP Version: 8.1.2
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
@@ -28,9 +28,6 @@ USE `bookstore`;
 --
 -- Table structure for table `cart`
 --
--- Creation: Jul 02, 2022 at 01:42 PM
--- Last update: Jul 03, 2022 at 09:22 PM
---
 
 DROP TABLE IF EXISTS `cart`;
 CREATE TABLE `cart` (
@@ -46,9 +43,6 @@ CREATE TABLE `cart` (
 
 --
 -- Table structure for table `message`
---
--- Creation: Jul 02, 2022 at 01:45 PM
--- Last update: Jul 03, 2022 at 09:17 PM
 --
 
 DROP TABLE IF EXISTS `message`;
@@ -66,15 +60,13 @@ CREATE TABLE `message` (
 --
 
 INSERT INTO `message` (`id`, `user_id`, `name`, `email`, `number`, `message`) VALUES
-(11, 5, 'Jane', 'jane@mail.com', '0923456678', 'hello');
+(11, 5, 'Jane', 'jane@mail.com', '0923456678', 'hello'),
+(12, 5, 'Jane', 'ST101@mail.com', '0447711', 'Hi testing this page');
 
 -- --------------------------------------------------------
 
 --
 -- Table structure for table `tbladmin`
---
--- Creation: Jul 02, 2022 at 05:41 PM
--- Last update: Jul 03, 2022 at 08:53 PM
 --
 
 DROP TABLE IF EXISTS `tbladmin`;
@@ -95,15 +87,13 @@ CREATE TABLE `tbladmin` (
 INSERT INTO `tbladmin` (`admin_id`, `admin_firstname`, `admin_lastname`, `username`, `admin_email`, `admin_password`, `admin_role`) VALUES
 (1, '', '', 'admin', 'admin@mail.com', '696d29e0940a4957748fe3fc9efd22a3', 'admin'),
 (2, NULL, NULL, 'janedoe', 'jane@mail.com', 'e10adc3949ba59abbe56e057f20f883e', 'pending'),
-(3, NULL, NULL, 'name', 'name@mail.com', '5f4dcc3b5aa765d61d8327deb882cf99', 'admin');
+(3, NULL, NULL, 'name', 'name@mail.com', '5f4dcc3b5aa765d61d8327deb882cf99', 'admin'),
+(6, NULL, NULL, 'fred', 'fred@mail.com', '5f4dcc3b5aa765d61d8327deb882cf99', 'admin');
 
 -- --------------------------------------------------------
 
 --
 -- Table structure for table `tblbook`
---
--- Creation: Jul 02, 2022 at 01:46 PM
--- Last update: Jul 03, 2022 at 08:41 PM
 --
 
 DROP TABLE IF EXISTS `tblbook`;
@@ -120,14 +110,20 @@ CREATE TABLE `tblbook` (
 
 INSERT INTO `tblbook` (`id`, `name`, `price`, `image`) VALUES
 (8, 'nightshade', 1200, 'nightshade.jpg'),
-(9, 'clever lands', 12000, 'clever_lands.jpg');
+(9, 'clever lands', 1200, 'clever_lands.jpg'),
+(10, 'programming logic and design', 500, 'prog.jpg'),
+(11, 'successfull project management', 800, 'succ.jpg'),
+(12, 'php programming with mySql', 1000, 'php.jpg'),
+(13, 'principles of information security', 250, 'inf.jpg'),
+(14, 'Basics of entrepreneurship', 350, 'ent.jpg'),
+(15, 'An introduction to scholarship', 500, 'scholar.jpg'),
+(16, 'java programming ', 750, 'java.jpg'),
+(17, 'effective business management', 800, 'eff.jpg');
 
 -- --------------------------------------------------------
 
 --
 -- Table structure for table `tblorder`
---
--- Creation: Jul 02, 2022 at 01:50 PM
 --
 
 DROP TABLE IF EXISTS `tblorder`;
@@ -145,13 +141,19 @@ CREATE TABLE `tblorder` (
   `payment_status` varchar(20) NOT NULL DEFAULT 'pending'
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
+--
+-- Dumping data for table `tblorder`
+--
+
+INSERT INTO `tblorder` (`order_id`, `user_id`, `name`, `number`, `email`, `method`, `address`, `total_products`, `total_price`, `placed_on`, `payment_status`) VALUES
+(11, 5, 'jane', '2', 'ST101@mail.com', 'cash on delivery', 'flat no. 7, streets, Port Elizabeth, South Africa - 6025', ', programming logic and design (1) ', 500, '04-Jul-2022', 'completed'),
+(12, 5, 'jane ', '0764178844', 'ST101@mail.com', 'paypal', 'flat no. 45, drier, Port Elizabeth, South Africa - 6245', ', nightshade (1) ', 1200, '04-Jul-2022', 'completed'),
+(13, 5, 'Mckyle Singh', '076415555', 'mckylesingh71@gmail.com', 'cash on delivery', 'flat no. 41, 47 wra, Port Elizabeth, South Africa - 6025', ', principles of information security (3) ', 750, '04-Jul-2022', 'completed');
+
 -- --------------------------------------------------------
 
 --
 -- Table structure for table `tbluser`
---
--- Creation: Jul 02, 2022 at 01:07 PM
--- Last update: Jul 03, 2022 at 08:52 PM
 --
 
 DROP TABLE IF EXISTS `tbluser`;
@@ -170,7 +172,8 @@ CREATE TABLE `tbluser` (
 --
 
 INSERT INTO `tbluser` (`user_id`, `user_firstname`, `studentnumber`, `username`, `user_role`, `user_password`, `user_status`) VALUES
-(5, '', 'ST101@mail.com', 'Jane', 'student', '5f4dcc3b5aa765d61d8327deb882cf99', 'verified');
+(5, '', 'ST101@mail.com', 'Jane', 'student', '5f4dcc3b5aa765d61d8327deb882cf99', 'verified'),
+(8, '', 'mckylesingh71@gmail.com', 'Mckyle Singh', 'student', '5f4dcc3b5aa765d61d8327deb882cf99', 'verfied');
 
 --
 -- Indexes for dumped tables
@@ -220,37 +223,37 @@ ALTER TABLE `tbluser`
 -- AUTO_INCREMENT for table `cart`
 --
 ALTER TABLE `cart`
-  MODIFY `id` int(100) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=61;
+  MODIFY `id` int(100) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=64;
 
 --
 -- AUTO_INCREMENT for table `message`
 --
 ALTER TABLE `message`
-  MODIFY `id` int(100) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=12;
+  MODIFY `id` int(100) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=13;
 
 --
 -- AUTO_INCREMENT for table `tbladmin`
 --
 ALTER TABLE `tbladmin`
-  MODIFY `admin_id` int(100) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
+  MODIFY `admin_id` int(100) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
 
 --
 -- AUTO_INCREMENT for table `tblbook`
 --
 ALTER TABLE `tblbook`
-  MODIFY `id` int(100) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=10;
+  MODIFY `id` int(100) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=18;
 
 --
 -- AUTO_INCREMENT for table `tblorder`
 --
 ALTER TABLE `tblorder`
-  MODIFY `order_id` int(100) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=11;
+  MODIFY `order_id` int(100) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=14;
 
 --
 -- AUTO_INCREMENT for table `tbluser`
 --
 ALTER TABLE `tbluser`
-  MODIFY `user_id` int(100) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
+  MODIFY `user_id` int(100) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=9;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
