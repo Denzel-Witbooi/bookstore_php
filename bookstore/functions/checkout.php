4512b814b8a77892371c844826878da076f67b1c
Denzel Witbooi <77748858+Denzel-Witbooi@users.noreply.github.com>
2022-07-04 19:05:58 +0200

    Update checkout.php

--- bookstore/functions/checkout.php
+++ bookstore/functions/checkout.php
@@ -27,7 +27,7 @@ function place_order($user_id)
      
         $total_products = implode(', ',$cart_products);
      
-        $order_query = mysqli_query($conn, "SELECT * FROM `orders` WHERE name = '$name' AND number = '$number' AND email = '$email' AND method = '$method' AND address = '$address' AND total_products = '$total_products' AND total_price = '$cart_total'") or die('query failed');
+        $order_query = mysqli_query($conn, "SELECT * FROM `tblorder` WHERE name = '$name' AND number = '$number' AND email = '$email' AND method = '$method' AND address = '$address' AND total_products = '$total_products' AND total_price = '$cart_total'") or die('query failed');
      
         if($cart_total == 0){
            $message[] = 'your cart is empty';
@@ -35,7 +35,7 @@ function place_order($user_id)
            if(mysqli_num_rows($order_query) > 0){
               $message[] = 'order already placed!'; 
            }else{
-              mysqli_query($conn, "INSERT INTO `orders`(user_id, name, number, email, method, address, total_products, total_price, placed_on) VALUES('$user_id', '$name', '$number', '$email', '$method', '$address', '$total_products', '$cart_total', '$placed_on')") or die('query failed');
+              mysqli_query($conn, "INSERT INTO `tblorder` (user_id, name, number, email, method, address, total_products, total_price, placed_on) VALUES('$user_id', '$name', '$number', '$email', '$method', '$address', '$total_products', '$cart_total', '$placed_on')") or die('query failed');
               $message[] = 'order placed successfully!';
               mysqli_query($conn, "DELETE FROM `cart` WHERE user_id = '$user_id'") or die('query failed');
            }
